e62a49e0f3b8f7da0969b81299a571493f06e38e
Ken Rettberg <kenrettberg@dbc-kenrettberg.local>
2016-11-29 15:57:54 -0800

    removed generator_spec gem

--- spec/frankly_spec.rb
+++ spec/frankly_spec.rb
@@ -5,7 +5,13 @@ describe Frankly do
     expect(Frankly::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "when generating a new skeleton" do
+    @templates_path = "#{File.dirname(__FILE__)}/../lib/sinatra_generator/templates"
+    @tmp_dir = Dir.mktmpdir
+    Dir.chdir @tmp_dir
+
+    it "does something useful" do
+      expect(@tmp_dir).to_not be nil
+    end
   end
 end
