1e5adf1976f21e8fde4e4f5d8b06dfdc6780283b
Ken Rettberg <kenrettberg@gmail.com>
2026-02-24 20:47:30 -0800

    Improve console and template generator/database defaults

--- templates/config/database.rb
+++ templates/config/database.rb
@@ -6,18 +6,27 @@ require "uri"
 rack_env = ENV.fetch("RACK_ENV", "development")
 ActiveRecord::Base.logger = Logger.new($stdout) if rack_env == "development"
 
-database_url = ENV.fetch("DATABASE_URL", "postgres://localhost/#{APP_NAME}_#{rack_env}")
-uri = URI.parse(database_url)
+build_db_config = lambda do |env_name|
+  database_url = ENV.fetch("DATABASE_URL", "postgres://localhost/#{APP_NAME}_#{env_name}")
+  uri = URI.parse(database_url)
+  database_name = uri.path.sub(%r{^/}, "")
+  adapter = uri.scheme == "postgres" ? "postgresql" : uri.scheme
 
-database_name = uri.path.sub(%r{^/}, "")
-adapter = uri.scheme == "postgres" ? "postgresql" : uri.scheme
+  {
+    adapter: adapter,
+    host: uri.host,
+    port: uri.port,
+    username: uri.user,
+    password: uri.password,
+    database: database_name,
+    encoding: "utf8"
+  }.compact
+end
 
-ActiveRecord::Base.establish_connection(
-  adapter: adapter,
-  host: uri.host,
-  port: uri.port,
-  username: uri.user,
-  password: uri.password,
-  database: database_name,
-  encoding: "utf8"
-)
+ActiveRecord::Base.configurations = {
+  "development" => build_db_config.call("development"),
+  "test" => build_db_config.call("test"),
+  "production" => build_db_config.call("production")
+}
+
+ActiveRecord::Base.establish_connection(rack_env.to_sym)
